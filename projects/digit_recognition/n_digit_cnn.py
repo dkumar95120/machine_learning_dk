721a79a992f8bc064a9693e046fc88160f2d5096
dkumar95120 <dkumar@gmail.com>
2017-03-14 15:00:04 -0700

    stable cnn and nn models

--- projects/digit_recognition/n_digit_cnn.py
+++ projects/digit_recognition/n_digit_cnn.py
@@ -36,50 +36,101 @@ print('train label shape', y_input.shape)
 print('valid label shape', y_valid.shape)
 print('tests label shape', y_tests.shape)
 
+patch_size = 5
+conv_stride= 1
+pool_size  = 2
+pool_stride= 2
+padding = 'SAME'
+
 def accuracy(y, labels):
 	return (100.0 * np.sum(np.argmax(y, 2).T == labels) / y.shape[1] / y.shape[0])
+def conv2d(x, W):
+	return tf.nn.conv2d(x, W, strides=[1, conv_stride, conv_stride, 1], padding=padding)
 
-def model (X, weights, biases, dropout=False):
+def max_pool_2x2(x):
+	return tf.nn.max_pool(x, ksize=[1, pool_size, pool_size, 1], strides=[1, pool_stride, pool_stride, 1], padding=padding)
+
+# Create Variables Function
+def weights_conv(shape, name):
+    return tf.get_variable(shape=shape, name=name,
+        initializer=tf.contrib.layers.xavier_initializer_conv2d())
+
+def weights_fc(shape, name):
+    return tf.get_variable(shape=shape, name=name,
+        initializer=tf.contrib.layers.xavier_initializer())
+
+def biases_var(shape, name):
+    return tf.Variable(tf.constant(0.1, shape=shape), name=name)
+
+def model (X, weights, biases, nconv, dropout=False):
 	#initialize relu_logits as the input dataset
 	nlayer = len(weights.keys())
-	#cur_logits = X.reshape(X.shape[0],X.shape[1]*X.shape[2]*X.shape[3])
-	cur_logits = X
-	for layer in range(nlayer):
+	hidden = X
+	# build convolution layers
+	for layer in range(nconv):
+		hidden = tf.nn.relu(conv2d(hidden, weights[layer]) + biases[layer])
+		hidden = max_pool_2x2(hidden)
+		if (dropout):
+			hidden = tf.nn.dropout(hidden, .8)
+
+	shape  = hidden.get_shape().as_list()
+	cur_logits = tf.reshape(hidden, [shape[0], shape[1] * shape[2] * shape[3]])
+
+	print ('First FC full dimensions', cur_logits.shape)
+
+	for layer in range(nconv, nlayer):
 		logits = tf.matmul(cur_logits, weights[layer]) + biases[layer]
 		# Transform logits computed from layer 1 using relu function to compute logits for layer2
 		if layer < nlayer-1:
 			cur_logits = tf.nn.relu(logits)
 		# Dropout on hidden layers only
 			if dropout:
-				cur_logits = tf.nn.dropout(cur_logits, 0.75)
+				cur_logits = tf.nn.dropout(cur_logits, 0.8)
 
 	return logits
 
+# Create Function for Image Size: Pooling
+# 3 Convolutions
+# 2 Max Pooling
+# final_image_size = output_size_pool(image_size, patch_size, conv_stride, pool_size, pool_stride, nlayer
+
+def output_size_pool(input_size, conv_filter_size, conv_stride, pool_filter_size, pool_stride, nlayer):
+
+    pad = 1.0 if padding == 'SAME' else 0.0
+
+    output = input_size
+    for layer in range(nlayer):
+	    # After convolution
+	    output = (((output - conv_filter_size + 2 * pad) / conv_stride) + 1.00)
+	    # After pool
+	    output = (((output - pool_filter_size + 2 * pad) / pool_stride) + 1.00)
+
+    return int(output)
+
 #Create a single hidden layer neural network using RELU and 1024 nodes
-def n_layer_nn (X_train, y_train, X_valid, y_valid, X_test, y_test, image, num_classes, ndigit,
-	batch_size=128, num_nodes=[1024], num_samples=0, num_steps = 1001, print_steps=100, dropout=False):
+def n_layer_cnn (X_train, y_train, X_valid, y_valid, X_test, y_test, image, num_classes, ndigit,
+				batch_size=128, num_samples=0, num_steps = 1001, print_steps=100, dropout=False):
 	beta = .01
 	if num_samples ==0:
 		num_samples = X_train.shape[0]
 
 	# reshape X_train, X_valid and X_test
-	X_train = X_train.reshape(X_train.shape[0],-1)
-	X_valid = X_valid.reshape(X_valid.shape[0],-1)
-	X_test  = X_test.reshape(X_test.shape[0],-1)
 	graph = tf.Graph()
 	#build nodes array and append it with the number of classes which is final number of nodes
-	nodes_list = num_nodes
-	nodes_list.append(num_classes)
+
+	batch_size = 64
+	image_size = image[0]
+	depth      = [ 8, 16, 32]
+	num_hidden = [64]
 
 	with graph.as_default():
 
 		# Input data. For the training data, we use a placeholder that will be fed
 		# at run time with a training minibatch.
-		tf_X_train = tf.placeholder(tf.float32, shape=(batch_size, image[0] * image[1] * image[2]))
+		tf_X_train = tf.placeholder(tf.float32, shape=(batch_size, image[0], image[1], image[2]))
 		tf_y_train = tf.placeholder(tf.int32, shape=(batch_size, ndigit))
-
-		tf_X_valid = tf.constant(X_valid.reshape(X_valid.shape[0],-1))
-		tf_X_test  = tf.constant(X_test.reshape(X_test.shape[0],-1))
+		tf_X_valid = tf.constant(X_valid)
+		tf_X_test  = tf.constant(X_test)
 
 		# save weights and biases in a dictionary for each layer
 		weights={}
@@ -89,19 +140,43 @@ def n_layer_nn (X_train, y_train, X_valid, y_valid, X_test, y_test, image, num_c
 		# first dimesion of the weights will be the number of features in the training dataset which is the image size
 		loss = 0
 		regularizers = 0
+		nconv = len(depth)
+		nfc   = len(num_hidden)
+		nlayer = nconv + nfc + 1
 
+		#build convolution dimensions
+		conv_dim = [num_channels] + depth + [num_hidden[0]]
+		print ("convolution dimensions", conv_dim)
+
+		# build FC layers dimensions
+		final_image_size = output_size_pool(image_size, patch_size, conv_stride, pool_size, pool_stride, len(depth))
+		hidden_dim = [final_image_size*final_image_size*depth[-1]] + num_hidden + [num_classes]
+		print ("hidden dimensions", hidden_dim)
+
+
+		print ('final image size', final_image_size)
 		for digit in range(ndigit):
-			dim1 = image[0] * image[1] * image[2]
 			weights[digit] = {}
 			biases[digit]  = {}
-			for layer, nodes in enumerate(nodes_list):
-				weights[digit][layer] = tf.Variable(tf.truncated_normal([dim1, nodes]))
-				biases [digit][layer] = tf.Variable(tf.zeros([nodes]))
+
+			# buld weights and biases for convolution layers first
+			for layer in range(nconv):
+				shape = [patch_size, patch_size, conv_dim[layer], conv_dim[layer+1]]
+				weights[digit][layer] = weights_conv(shape,'w'+str(digit)+str(layer))
+				biases[digit][layer]  = biases_var([depth[layer]],'b'+str(digit)+str(layer))
 				w_l2_loss = tf.nn.l2_loss(weights[digit][layer])
 				regularizers = regularizers + w_l2_loss
-				# Subsequent layer first dimension would the number of nodes in the previous layer
-				dim1 = nodes
-			logits[digit] = model (tf_X_train, weights[digit], biases[digit], dropout)
+
+			# build weights and biases for fully connected layers next
+			for layer in range (nconv,nlayer):
+				i = layer - nconv # index to hidden layer
+				shape = [hidden_dim[i], hidden_dim[i+1]]
+				weights[digit][layer] = weights_fc(shape,'w'+str(digit)+str(layer))
+				biases[digit][layer]  = biases_var([hidden_dim[i+1]], 'b'+str(digit)+str(layer))
+				w_l2_loss = tf.nn.l2_loss(weights[digit][layer])
+				regularizers = regularizers + w_l2_loss
+
+			logits[digit] = model (tf_X_train, weights[digit], biases[digit], nconv, dropout)
 			loss += tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(labels=tf_y_train[:,digit], logits=logits[digit]))
 
 		# Loss function with L2 Regularization with beta
@@ -109,7 +184,7 @@ def n_layer_nn (X_train, y_train, X_valid, y_valid, X_test, y_test, image, num_c
 
 		# Optimize using Gradient Descent
 		global_step = tf.Variable(0)  # count the number of steps taken.
-		init_learning_rate = 0.05
+		init_learning_rate = 0.01
 		learning_rate = tf.train.exponential_decay(init_learning_rate, global_step, decay_steps=100000, decay_rate=0.95, staircase=True)
 		#optimizer = tf.train.GradientDescentOptimizer(learning_rate).minimize(loss, global_step=global_step)
 		optimizer = tf.train.AdagradOptimizer(learning_rate).minimize(loss, global_step=global_step)
@@ -123,14 +198,16 @@ def n_layer_nn (X_train, y_train, X_valid, y_valid, X_test, y_test, image, num_c
 
 		# Transform validation samples using the same transformation used above for training samples
 		for digit in range(ndigit):
-			valid_logits = model (tf_X_valid, weights[digit], biases[digit], dropout)
+			valid_logits = model (tf_X_valid, weights[digit], biases[digit], nconv, dropout)
 			y_pred[digit] = tf.nn.softmax(valid_logits)
+
 		y_valid_pred = tf.stack([y_pred[0],y_pred[1], y_pred[2], y_pred[3]])
 
 		# Transform test samples using the same transformation used above for training samples
 		for digit in range(ndigit):
-			test_logits = model (tf_X_test, weights[digit], biases[digit], dropout)
+			test_logits = model (tf_X_test, weights[digit], biases[digit], nconv, dropout)
 			y_pred[digit] = tf.nn.softmax(test_logits)
+
 		y_test_pred = tf.stack([y_pred[0],y_pred[1], y_pred[2], y_pred[3]])
 
 
@@ -165,21 +242,29 @@ def n_layer_nn (X_train, y_train, X_valid, y_valid, X_test, y_test, image, num_c
 				accuracy(y_valid_eval, y_valid)))
 				# Calling .eval() on valid_prediction is basically like calling run()
 		y_test_eval = y_test_pred.eval()
+
 		print('Test accuracy: {:.1f}'.format(accuracy(y_test_eval, y_test)))
 	return y_test_eval, y_valid_eval
 
 image_size = X_train.shape[1]
 batch_size = 64
-num_nodes  = [4096]
 num_channels = X_train.shape[3]
 image = [image_size, image_size, num_channels]
 print ('image size',image)
 nclass = num_labels
-num_steps = 5001
-print_steps = 500
+num_steps = 1001
+print_steps = 100
 ndigit = 4 # our synthetic data has only 4 digits
 dropout = False
 num_samples=0 # try with full dataset first
-y_test_pred, y_valid_pred = n_layer_nn (X_input, y_input, X_valid, y_valid, X_tests, y_tests, image, nclass, ndigit,
-									            batch_size, num_nodes, num_samples, num_steps, print_steps, dropout)
+
+patch_size = 5
+conv_stride= 1
+pool_size  = 2
+pool_stride= 2
+padopt = padding
+conv_params = [patch_size, conv_stride, pool_size, pool_stride, padopt]
+
+y_test_pred, y_valid_pred = n_layer_cnn (X_input, y_input, X_valid, y_valid, X_tests, y_tests, image, nclass, ndigit,
+									            batch_size, num_samples, num_steps, print_steps, dropout)
 
